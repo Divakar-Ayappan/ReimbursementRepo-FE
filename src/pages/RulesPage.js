664f436d4fe789e5db63b04ca35b20ac60c7c405
Divakar A <adivakau@amazon.com>
2025-06-23 22:11:07 +0530

    Fixed modal not opening in the rules page issue. - Passed the attributes that were required for the modal.

--- src/pages/RulesPage.js
+++ src/pages/RulesPage.js
@@ -7,33 +7,34 @@ import {toast} from "react-toastify";
 import PageWithModal from "../modals/PageWithModal";
 
 function RulesPage() {
-    const { isOpen, setIsOpen } = useOutletContext();
+    const { isRFormOpen, setIsRFormOpen } = useOutletContext();
     const queryClient = useQueryClient();
 
     const handleFormSubmit = async (formData) => {
         try {
             await createReimbursementRequest(formData);
             toast.success("Request submitted");
-            setIsOpen(false);
+            isRFormOpen(false);
             queryClient.invalidateQueries(['requests']);
         } catch (err) {
             toast.error("Failed to submit request");
         }
     };
 
-    const { data, isLoading, error } = useQuery({
+    const { data: rules, isLoading, error } = useQuery({
         queryKey: ['rules', { rulesFilter: "ACTIVE" }],
         queryFn: () => getAllRules({ rulesFilter: "ACTIVE" }),
         onError: () => toast.error('Failed to load rules'),
     });
     if (isLoading) return <p>Loading rules...</p>;
     if (error) return <p>Error loading rules</p>;
-    if (!data || data.length === 0) return <p>No rules available</p>;
+    if (!rules || rules.length === 0) return <p>No rules available</p>;
 
     return (
-        <PageWithModal isOpen={isOpen} setIsOpen={setIsOpen} onSubmit={handleFormSubmit}>
+        <PageWithModal isRFormOpen={isRFormOpen} setIsRFormOpen={setIsRFormOpen}
+                       onSubmit={handleFormSubmit} rules={rules} setFormOpeningMode={()=> ''}>
             <div className={styles.rulesPage}>
-                {data.map(rule => (
+                {rules.map(rule => (
                     <RulesCard key={rule.ruleId} rule={rule} />
                 ))}
             </div>
